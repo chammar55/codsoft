57ad4087f976fb4e9b7af2bb0455863fb262246d
chammar55 <chammar5255@gmail.com>
2023-11-09 14:43:12 +0500

    m

---
+++ blogging-plateform-mern_task-6/blog-final/backend/routes/users.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const router = express.Router();
+const User = require("../models/User");
+const bcrypt = require("bcrypt");
+
+// UPDATE
+router.put("/:id", async (req, res) => {
+  try {
+    if (req.body.password) {
+      const salt = await bcrypt.genSalt(10);
+      req.body.password = await bcrypt.hashSync(req.body.password, salt);
+    }
+    const updatedUser = await User.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: req.body,
+      },
+      { new: true }
+    );
+    req.status(200).json(updatedUser);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+//DELETE
+
+// DET USER
